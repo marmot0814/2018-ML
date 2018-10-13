d512522fffdab233f66ea1124cd1f9e397ead301
Marmot Yang <s1357andy871207@gmail.com>
2018-10-14 01:53:03 +0800

    100Fold100Tree go

--- HW1/google-play-store-apps/solve.py
+++ HW1/google-play-store-apps/solve.py
@@ -5,6 +5,6 @@ from data_processing import Forest
 forest = Forest('data/new_googleplaystore.csv')
 data, target, test_data, test_target = forest.create_data()
 
-scores, avg_score = forest.KFold(5, tree_cnt=20)
+scores, avg_score = forest.KFold(100, tree_cnt=100)
 print(scores)
 print(avg_score)
