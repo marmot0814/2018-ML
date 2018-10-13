aebe10e6e82efb76a01b98ca7aabd5f2e688ee77
karljackab <karljackab@gmail.com>
2018-10-14 01:36:00 +0800

    fix outer merge of merge user google

--- HW1/google-play-store-apps/data/merge_user_google.py
+++ HW1/google-play-store-apps/data/merge_user_google.py
@@ -9,7 +9,11 @@ count = data.groupby('App')['Sentiment_Polarity'].count().to_frame()
 polarity = polarity.rename(columns={'Sentiment_Polarity':'comment_mean'})
 count = count.rename(columns={'Sentiment_Polarity':'comment_count'})
 
-res = pd.merge(data2, polarity, on = 'App')
-res = pd.merge(res, count, on ='App')
+res = pd.merge(data2, polarity, on = 'App', how='outer')
+res = pd.merge(res, count, on ='App', how='outer')
+
+null_data = res.isnull()
+res.loc[null_data.iloc[:,-2],'comment_mean']=0
+res.loc[null_data.iloc[:,-1],'comment_count']=0
 
 res.to_csv('merged_googleplaystore.csv',index=False)
