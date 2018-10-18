d020ea17c4af56f56191f94debdda44abd14ab0b
karljackab <karljackab@gmail.com>
2018-10-18 10:32:31 +0800

    add Iris.py some text

--- HW1/iris/Iris.py
+++ HW1/iris/Iris.py
@@ -4,8 +4,11 @@ forest = Forest(
     'data.csv',
     label="sepal length,sepal width,petal length,petal width,outcome\n")
 data, target, test_data, test_target = forest.create_data()
-scores, avg_score = forest.KFold(2, tree_cnt=1)
-print(scores)
-print(avg_score)
+
+
+K = 2
+scores, avg_score = forest.KFold(K, tree_cnt=1)
+print(f'{K}-folds score: {scores}')
+print(f'average score: {avg_score}')
 clfs = forest.create_trees(data, target, tree_cnt=1)
 print(forest.predict(clfs, [[5.1, 3.5, 1.4, 0.2]]))
