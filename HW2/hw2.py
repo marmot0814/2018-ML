34c493b7244376e0adcb382b5caededbe9d67b45
Darklanx <csdarklanx@gmail.com>
2018-10-27 19:18:26 +0800

    figure 1

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,10 +1,12 @@
 from process import process
 import numpy as np
 import pandas as pd
-
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
 # data processing
 df = pd.read_csv('./datasets/data_noah.csv')[['x', 'y', 'pitch_type']]
 data = np.array([[df['x'].values[i], df['y'].values[i]]
                  for i in range(df['x'].values.shape[0])])
 
-result = process.Kmeans(data, k = 3)
+result = process.Kmeans(data, k=3)
--- HW2/process/process.py
+++ HW2/process/process.py
@@ -1,10 +1,13 @@
 import numpy as np
 import time
+import matplotlib
+matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
+
 
 class Kmeans:
 
-    def __init__(self, data, k = 3):
+    def __init__(self, data, k=3):
 
         self.n = data.shape[0]
         self.k = k
@@ -17,56 +20,62 @@ class Kmeans:
         # initial plt
         self.fig, self.ax = plt.subplots()
         plt.ion()
-        self.fig = plt.figure()
+        plt.show()
+        self.fig = plt.figure(1)
         self.ax = self.fig.add_subplot(111)
 
         # initial centers
-        self.centers = np.array([ self.data[i] for i in range(self.k) ])
+        self.centers = np.array([self.data[i] for i in range(self.k)])
 
         # inital clusters
-        self.clusters = [ np.array([]) for x in range(self.k) ]
+        self.clusters = [np.array([]) for x in range(self.k)]
 
         # iterator
         while True:
             stop = self.iterator()
-            time.sleep(0.5)
+            #time.sleep(0.5)
             self.display()
+            plt.pause(0.5)
+
             if stop:
-                break;
+                break
 
     def iterator(self):
 
-        # initial 
-        clusters = [ [] for x in range(self.k) ]
+        # initial
+        clusters = [[] for x in range(self.k)]
 
         # find the cluster of the data for each one
         for i in range(self.n):
-            clusters[np.argmin(self.distance(self.centers, self.data[i]))].append(self.data[i])
+            clusters[np.argmin(self.distance(
+                self.centers, self.data[i]))].append(self.data[i])
 
         # trans list to numpy array
-        clusters = [ np.array(clusters[i]) for i in range(self.k) ]
+        clusters = [np.array(clusters[i]) for i in range(self.k)]
 
         # update centers
-        self.centers = np.array([np.mean( clusters[i], axis = 0) for i in range(self.k) ])
+        self.centers = np.array(
+            [np.mean(clusters[i], axis=0) for i in range(self.k)])
 
         # swap old and new clusters
         self.clusters, clusters = clusters, self.clusters
 
-        return all([ np.array_equal(x, y) for x, y in zip(self.clusters, clusters) ])
+        return all(
+            [np.array_equal(x, y) for x, y in zip(self.clusters, clusters)])
 
     def distance(self, a, b):
-        return np.sum( (a - b) ** 2, axis = 1)
+        return np.sum((a - b)**2, axis=1)
 
     def display(self):
         for i in range(self.k):
             self.ax.plot([x[0] for x in self.clusters[i]],
-                    [x[1] for x in self.clusters[i]],
-                     marker = 'o', linestyle='', ms = 4, color = self.color[i])
+                         [x[1] for x in self.clusters[i]],
+                         marker='o',
+                         linestyle='',
+                         ms=4,
+                         color=self.color[i])
 
         self.fig.canvas.draw()
 
     def cost(self):
         pass
-
-
-
