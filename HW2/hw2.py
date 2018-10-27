285dcbdca18883e8a4067b7901fd26abf949ffc9
Marmot Yang <s1357andy871207@gmail.com>
2018-10-27 18:29:20 +0800

    plot ok

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,4 +1,4 @@
-from process import test
+from process import process
 import numpy as np
 import pandas as pd
 
@@ -7,4 +7,4 @@ df = pd.read_csv('./datasets/data_noah.csv')[['x', 'y', 'pitch_type']]
 data = np.array([[df['x'].values[i], df['y'].values[i]]
                  for i in range(df['x'].values.shape[0])])
 
-result = test.Kmeans(data, k = 20)
+result = process.Kmeans(data, k = 3)
--- HW2/process/process.py
+++ HW2/process/process.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from matplotlib import pyplot as plt
 
 class Kmeans:
@@ -7,10 +8,17 @@ class Kmeans:
 
         self.n = data.shape[0]
         self.k = k
+        self.color = ['r', 'g', 'b', 'y']
 
         # shuffle data
         indices = np.random.permutation(self.n)
         self.data = np.array(data[indices])
+
+        # initial plt
+        self.fig, self.ax = plt.subplots()
+        plt.ion()
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
 
         # initial centers
         self.centers = np.array([ self.data[i] for i in range(self.k) ])
@@ -21,6 +29,7 @@ class Kmeans:
         # iterator
         while True:
             stop = self.iterator()
+            time.sleep(0.5)
             self.display()
             if stop:
                 break;
@@ -43,22 +52,18 @@ class Kmeans:
         # swap old and new clusters
         self.clusters, clusters = clusters, self.clusters
 
-        return all([ np.array_equal(x, y) for x, y in zip(self.clusters, clusters)])
+        return all([ np.array_equal(x, y) for x, y in zip(self.clusters, clusters) ])
 
     def distance(self, a, b):
         return np.sum( (a - b) ** 2, axis = 1)
 
     def display(self):
-        fig, ax = plt.subplots()
         for i in range(self.k):
-            ax.plot([x[0] for x in self.clusters[i]],
+            self.ax.plot([x[0] for x in self.clusters[i]],
                     [x[1] for x in self.clusters[i]],
-                     marker = 'o', linestyle='', ms = 4)
-        for i in range(self.k):
-            ax.plot(self.centers[i][0], self.centers[i][1], marker = '+', ms = 10, color = 'r't )
+                     marker = 'o', linestyle='', ms = 4, color = self.color[i])
 
-        fig.canvas.draw()
-        plt.show()
+        self.fig.canvas.draw()
 
     def cost(self):
         pass
