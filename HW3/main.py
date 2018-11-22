726133635c4905568bd5ced24ea861f294ce732b
alter027 <wsjyun749@gmail.com>
2018-11-23 00:08:43 +0800

    Update main.py

--- HW3/main.py
+++ HW3/main.py
@@ -27,29 +27,13 @@ def gradient_descent(X, Y, learning_rate, epoch):
 		b -= learning_rate*db
 		# print(da, db, a, b, cost)
 	return a, b
-
 def p1(datas, keys, ind):
 	for i in range(len(keys)):
 		data = np.array(datas)[:,i].astype(np.float)
 		pidt = np.array(datas)[:,ind].astype(np.float)
-
 		lm = LinearRegression()
 		lm.fit(np.reshape(data,(len(data),1)),np.reshape(pidt,(len(pidt),1)))
 		print(lm.coef_,lm.intercept_,keys[i]) # 印出係數 截距
-		'''
-		# 新的氣溫
-		to_be_predicted = np.array([30])
-		predicted_sales = lm.predict(np.reshape(to_be_predicted, (len(to_be_predicted), 1)))
-		# 預測的冰紅茶銷量
-		print(predicted_sales)
-		# 視覺化
-		plt.scatter(data, pidt, color='black')
-		plt.plot(data, lm.predict(np.reshape(data, (len(data), 1))), color='blue', linewidth=3)
-		# plt.plot(to_be_predicted, predicted_sales, color = 'red', marker = '^', markersize = 10)
-		ax = plt.gca()
-		ax.set_aspect(1)
-		plt.show()
-		'''
 def p2(datas, keys, ind, epoch):
 	for i in range(len(keys)):
 	# for i in range(1):
