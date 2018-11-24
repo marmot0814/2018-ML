e5741c44f6b9d65b49dca9595febb2598f83ca66
karljackab <karljackab@gmail.com>
2018-11-25 03:57:42 +0800

    add p1 r2_score,weight,bias,graph

--- HW3/main.py
+++ HW3/main.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 import numpy as np
 import csv
+import random
 
 MPa = 'Concrete compressive strength(MPa, megapascals) '
 
@@ -11,7 +12,12 @@ def load_file(filename):
 		reader = list(csv.DictReader(csvfile))
 		datas = [list(i.values()) for i in reader]
 		keys = list(reader[0].keys())
-		return datas, keys, keys.index(MPa)
+
+		train_datas = datas[0:int(0.8*len(datas))]
+		test_datas = datas[int(0.8*len(datas)):]
+
+		return train_datas, test_datas, keys, keys.index(MPa)
+
 def gradient_descent(X, Y, learning_rate, epoch):
 	a, b = 0.0, 0.0
 	num = len(Y)
@@ -27,13 +33,61 @@ def gradient_descent(X, Y, learning_rate, epoch):
 		b -= learning_rate*db
 		# print(da, db, a, b, cost)
 	return a, b
-def p1(datas, keys, index):
-	for i in range(len(keys)):
-		data = np.array(datas)[:,i].astype(np.float)
-		pidt = np.array(datas)[:,index].astype(np.float)
-		lm = LinearRegression()
-		lm.fit(np.reshape(data,(len(data),1)),np.reshape(pidt,(len(pidt),1)))
-		print(lm.coef_,lm.intercept_,keys[i]) # 印出係數 截距
+
+def plot(X, Y, weight, bias, keys):
+	frames = len(weight)
+	X = np.array(X).astype(np.float)
+	Y = np.array(Y).astype(np.float)
+	weight = np.array(weight).astype(np.float)
+	bias = np.array(bias).astype(np.float)
+
+	min_X = np.amin(X, axis=0)
+	max_X = np.amax(X, axis=0)
+
+	for frame in range(frames):
+		plt.subplot(2,4,frame+1)
+
+		minx = min_X[frame]
+		maxx = max_X[frame]
+		
+		if minx < 0:	# expand line length
+			minx*=1.1
+		else:
+			minx*=0.9
+
+		if maxx>0:
+			maxx*=1.1
+		else:
+			maxx*=0.9
+
+		plt.plot([minx,maxx],[minx*weight[frame]+bias, maxx*weight[frame]+bias])
+
+		plt.scatter(np.reshape(X[:,frame],(1,len(X))),Y,s=5)
+		
+		plt.xlabel(keys[frame].split('(')[0])
+		plt.tight_layout()
+	plt.show()
+
+def p1(datas, test_datas, keys, index):
+	datas = np.array(datas).astype(np.float)	#training data
+	train_data = datas[:,0:len(keys)-1]
+	const_one_arr = np.ones((len(train_data),1))
+	train_data = np.append(const_one_arr, train_data, axis=1)	#append const column
+	target_data = datas[:,index]	#training target data
+	lm = LinearRegression().fit(train_data, target_data)
+
+	test_datas = np.array(test_datas).astype(np.float)	#testing data
+	test_data = test_datas[:,0:len(keys)-1]
+	const_one_arr = np.ones((len(test_datas),1))
+	test_data = np.append(const_one_arr, test_data, axis=1)
+	test_target_data = test_datas[:,index]	#testing target data
+
+	print("r2_score: ", lm.score(test_data, test_target_data))
+	print("weight: ", lm.coef_[1:])
+	print("bias: ",lm.coef_[0])
+
+	plot(test_data[:,1:], test_target_data, lm.coef_[1:], lm.coef_[0], keys)
+
 def p2(datas, keys, index, epoch):
 	for i in range(len(keys)):
 	# for i in range(1):
@@ -43,14 +97,13 @@ def p2(datas, keys, index, epoch):
 		print(a, b, keys[i])
 
 def main():
-	datas, keys, index = load_file('Concrete_Data.csv')
-	# for i in range(len(keys)):
+	train_datas, test_datas, keys, index = load_file('Concrete_Data.csv')
 	print('Problem 1:')
-	p1(datas, keys, index)
+	p1(train_datas, test_datas, keys, index)
 	print('=============================')
-	epoch = 100000
-	print('Problem 2:')
-	p2(datas, keys, index, epoch)
+	# epoch = 100000
+	# print('Problem 2:')
+	# p2(datas, keys, index, epoch)
 
 if __name__ == "__main__":
     main()
