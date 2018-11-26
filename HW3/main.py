edd0570539c138f06a0040fa84dfb9df14c8b35d
Darklanx <csdarklanx@gmail.com>
2018-11-26 17:19:04 +0800

    p1 add shuffle & fix bias

--- HW3/main.py
+++ HW3/main.py
@@ -7,103 +7,116 @@ import random
 
 MPa = 'Concrete compressive strength(MPa, megapascals) '
 
+
 def load_file(filename):
-	with open(filename, newline='') as csvfile:
-		reader = list(csv.DictReader(csvfile))
-		datas = [list(i.values()) for i in reader]
-		keys = list(reader[0].keys())
+    with open(filename, newline='') as csvfile:
+        reader = list(csv.DictReader(csvfile))
+        datas = [list(i.values()) for i in reader]
+        keys = list(reader[0].keys())
 
-		train_datas = datas[0:int(0.8*len(datas))]
-		test_datas = datas[int(0.8*len(datas)):]
+        train_datas = datas[0:int(0.8 * len(datas))]
+        test_datas = datas[int(0.8 * len(datas)):]
 
-		return train_datas, test_datas, keys, keys.index(MPa)
+        return train_datas, test_datas, keys, keys.index(MPa)
+
 
 def gradient_descent(X, Y, learning_rate, epoch):
-	a, b = 0.0, 0.0
-	num = len(Y)
-	for i in range(epoch):
-		y = a*X + b
-		cost = sum([i**2 for i in (Y-y)])/num
-		da = -(2/num)*sum(X*(Y-y))
-		db = -(2/num)*sum(Y-y)
-		# softmax # overflow
-		da = max(da, -1) if da < 0 else min(da,1)
-		db = max(db, -1) if db < 0 else min(db,1)
-		a -= learning_rate*da
-		b -= learning_rate*db
-		# print(da, db, a, b, cost)
-	return a, b
+    a, b = 0.0, 0.0
+    num = len(Y)
+    for i in range(epoch):
+        y = a * X + b
+        cost = sum([i**2 for i in (Y - y)]) / num
+        da = -(2 / num) * sum(X * (Y - y))
+        db = -(2 / num) * sum(Y - y)
+        # softmax # overflow
+        da = max(da, -1) if da < 0 else min(da, 1)
+        db = max(db, -1) if db < 0 else min(db, 1)
+        a -= learning_rate * da
+        b -= learning_rate * db
+        # print(da, db, a, b, cost)
+    return a, b
+
 
 def plot(X, Y, weight, bias, keys):
-	frames = len(weight)
-	X = np.array(X).astype(np.float)
-	Y = np.array(Y).astype(np.float)
-	weight = np.array(weight).astype(np.float)
-	bias = np.array(bias).astype(np.float)
+    frames = len(weight)
+    X = np.array(X).astype(np.float)
+    Y = np.array(Y).astype(np.float)
+    weight = np.array(weight).astype(np.float)
+    bias = np.array(bias).astype(np.float)
 
-	min_X = np.amin(X, axis=0)
-	max_X = np.amax(X, axis=0)
+    min_X = np.amin(X, axis=0)
+    max_X = np.amax(X, axis=0)
 
-	for frame in range(frames):
-		plt.subplot(2,4,frame+1)
+    for frame in range(frames):
+        plt.subplot(2, 4, frame + 1)
 
-		minx = min_X[frame]
-		maxx = max_X[frame]
-		
-		if minx < 0:	# expand line length
-			minx*=1.1
-		else:
-			minx*=0.9
+        minx = min_X[frame]
+        maxx = max_X[frame]
 
-		if maxx>0:
-			maxx*=1.1
-		else:
-			maxx*=0.9
+        if minx < 0:  # expand line length
+            minx *= 1.1
+        else:
+            minx *= 0.9
 
-		plt.plot([minx,maxx],[minx*weight[frame]+bias, maxx*weight[frame]+bias])
+        if maxx > 0:
+            maxx *= 1.1
+        else:
+            maxx *= 0.9
 
-		plt.scatter(np.reshape(X[:,frame],(1,len(X))),Y,s=5)
-		
-		plt.xlabel(keys[frame].split('(')[0])
-		plt.tight_layout()
-	plt.show()
+        plt.plot([minx, maxx],
+                 [minx * weight[frame] + bias, maxx * weight[frame] + bias])
+
+        plt.scatter(np.reshape(X[:, frame], (1, len(X))), Y, s=5)
+
+        plt.xlabel(keys[frame].split('(')[0])
+        plt.tight_layout()
+    plt.show()
+
 
 def p1(datas, test_datas, keys, index):
-	datas = np.array(datas).astype(np.float)	#training data
-	train_data = datas[:,0:len(keys)-1]
-	const_one_arr = np.ones((len(train_data),1))
-	train_data = np.append(const_one_arr, train_data, axis=1)	#append const column
-	target_data = datas[:,index]	#training target data
-	lm = LinearRegression().fit(train_data, target_data)
+    datas = np.array(datas).astype(np.float)  #training data
+    train_data = datas[:, 0:len(keys) - 1]
+    """
+    const_one_arr = np.ones((len(train_data), 1))
+    train_data = np.append(
+        const_one_arr, train_data, axis=1)  #append const column
+    """
+    target_data = datas[:, index]  #training target data
+    np.random.shuffle(train_data)
+    lm = LinearRegression().fit(train_data, target_data)
+    test_datas = np.array(test_datas).astype(np.float)  #testing data
+    test_data = test_datas[:, 0:len(keys) - 1]
+    """
+    const_one_arr = np.ones((len(test_datas), 1))
+    test_data = np.append(const_one_arr, test_data, axis=1)
+    """
+    test_target_data = test_datas[:, index]  #testing target data
 
-	test_datas = np.array(test_datas).astype(np.float)	#testing data
-	test_data = test_datas[:,0:len(keys)-1]
-	const_one_arr = np.ones((len(test_datas),1))
-	test_data = np.append(const_one_arr, test_data, axis=1)
-	test_target_data = test_datas[:,index]	#testing target data
+    print("r2_score: ", lm.score(test_data, test_target_data))
+    print("weight: ", lm.coef_[0:])
+    print("bias: ", lm.intercept_)
 
-	print("r2_score: ", lm.score(test_data, test_target_data))
-	print("weight: ", lm.coef_[1:])
-	print("bias: ",lm.coef_[0])
+    plot(test_data[:, 1:], test_target_data, lm.coef_[1:], lm.coef_[0], keys)
 
-	plot(test_data[:,1:], test_target_data, lm.coef_[1:], lm.coef_[0], keys)
 
 def p2(datas, keys, index, epoch):
-	for i in range(len(keys)):
-	# for i in range(1):
-		data = np.array(datas)[:,i].astype(np.float)
-		pidt = np.array(datas)[:,index].astype(np.float)
-		a, b = gradient_descent(data, pidt, 0.001, epoch)
-		print(a, b, keys[i])
+    for i in range(len(keys)):
+        # for i in range(1):
+        data = np.array(datas)[:, i].astype(np.float)
+        pidt = np.array(datas)[:, index].astype(np.float)
+        a, b = gradient_descent(data, pidt, 0.001, epoch)
+        print(a, b, keys[i])
+
 
 def main():
-	train_datas, test_datas, keys, index = load_file('Concrete_Data.csv')
-	print('Problem 1:')
-	p1(train_datas, test_datas, keys, index)
-	print('=============================')
-	# epoch = 100000
-	# print('Problem 2:')
-	# p2(datas, keys, index, epoch)
+    train_datas, test_datas, keys, index = load_file('Concrete_Data.csv')
+    print('Problem 1:')
+    p1(train_datas, test_datas, keys, index)
+    print('=============================')
+    # epoch = 100000
+    # print('Problem 2:')
+    # p2(datas, keys, index, epoch)
+
 
 if __name__ == "__main__":
     main()
