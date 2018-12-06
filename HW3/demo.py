c8c1eac0fc8d779b12a8de595f9306c3768f71e4
Marmot Yang <s1357andy871207@gmail.com>
2018-12-07 03:36:22 +0800

    auto push

--- HW3/demo.py
+++ HW3/demo.py
@@ -31,9 +31,12 @@ def load_file(filename, visualized = True):
 def MSE(y, Y):
     return np.sum(np.square(y - Y)) / y.shape[0]
 
+def LogCosh(y, Y):
+    return np.sum(np.log(np.cosh(y - Y)))
+
 def R2(predict_y, test_Y):
     y_mean = np.mean(test_Y)
-    _R2 = 1 - np.sum(np.square(predict_y - y_mean)) / np.sum(
+    _R2 = 1 - np.sum(np.square(predict_y - test_Y)) / np.sum(
         np.square(test_Y - y_mean))
     return _R2
 
@@ -130,11 +133,18 @@ def p2(train_data, test_data):
     plt.show()
 
 
-def MVGD(X, Y, lr, epoch, test_X, test_Y, error_min, error_max, visualized = True, xlabel = '', ylabel = ''):
-    base = 0
+def MVGD(X, Y, lr, epoch, test_X, test_Y, optimizer, error_min, error_max, xlabel = '', ylabel = '', lossFunction = 'MSE'):
     w = 0.002 * np.random.random_sample(X.shape[1]) - 0.001
     eps = 1e-8
-    G = np.zeros(len(w))
+
+    if optimizer == 'adagrad':
+        G = np.zeros(len(w))
+
+    if optimizer == 'adam':
+        M = np.zeros(len(w))
+        V = np.zeros(len(w))
+        beta_1 = 0.9
+        beta_2 = 0.999
 
     train_error = []
     test_error = []
@@ -145,24 +155,29 @@ def MVGD(X, Y, lr, epoch, test_X, test_Y, error_min, error_max, visualized = Tru
     error_fig = plt.figure()
     error_plot = error_fig.add_subplot(111)
 
-    if visualized:
-        point_fig = plt.figure()
-        point_plot = point_fig.add_subplot(111)
-
-    output_density = 100
+    output_density = 200
 
     for i in range(epoch):
         y = np.dot(X, w).reshape(X.shape[0], 1)
         test_y = np.dot(test_X, w).reshape(test_X.shape[0], 1)
-        dw = -1 * sum(Y - y) * w
+
+        if lossFunction == 'MSE':
+            dw = -1 * (Y - y).T.dot(X).reshape(X.shape[1]) / X.shape[0]
+        if lossFunction == 'LogCosh':
+            dw = ((np.sinh(Y - y) / np.cosh(Y - y)).T.dot(X)).reshape(X.shape[1])
+
 
         if i % output_density == 0:
             error_plot.cla()
 
-            train_err = MSE(y, Y)
-            test_err = MSE(test_y, test_Y)
-            if len(train_error) != 0 and abs(train_error[-1] - train_err) < 0.01:
-                break
+            if lossFunction == 'MSE':
+                train_err = MSE(y, Y)
+                test_err = MSE(test_y, test_Y)
+
+            if lossFunction == 'LogCosh':
+                train_err = LogCosh(y, Y)
+                test_err = LogCosh(test_y, test_Y)
+
 
             train_error.append(train_err)
             test_error.append(test_err)
@@ -170,69 +185,58 @@ def MVGD(X, Y, lr, epoch, test_X, test_Y, error_min, error_max, visualized = Tru
             error_plot.plot(list(range(i // output_density + 1)), train_error[0: (i // output_density) + 1])
             error_plot.plot(list(range(i // output_density + 1)), test_error[0:(i//output_density) + 1])
             error_plot.set_xlabel('iteration(* {} epoch)'.format(output_density))
-            error_plot.set_ylabel('loss(MSE)')
+
+            if lossFunction == 'MSE':
+                error_plot.set_ylabel('loss(MSE)')
+
+            if lossFunction == 'LogCosh':
+                error_plot.set_ylabel('loss(LogCosh)')
+
             error_plot.set_ylim([0, error_max])
             error_fig.canvas.draw()
-            if visualized:
-                point_plot.cla()
-                point_plot.plot(test_X[:, 1], test_Y, '.')
-                minx = min(test_X[:, 1])
-                maxx = max(test_X[:, 1])
-                point_plot.plot(
-                    np.linspace(minx,maxx,100),
-                    Polynomialize(np.linspace(minx,maxx,100).reshape(100, 1), X.shape[1] - 1).dot(w)
-                )
-                point_plot.set_xlabel(xlabel, fontsize = 10)
-                point_plot.set_ylabel(ylabel, fontsize = 10)
-                point_plot.text(
-                    x = (minx + maxx) / 2,
-                    y = test_Y.mean() + 20,
-                    s = "loss = {}\nr2 = {}".format(MSE(test_y, test_Y), R2(test_y, test_Y)),
-                    fontsize=12
-                )
-                point_plot.axis("equal")
-                point_fig.canvas.draw()
 
-        train_err = MSE(y, Y)
-        if train_err >= error_max or (i - base >= 1000 and train_err >= 1000):
-            w = 0.002 * np.random.random_sample(X.shape[1],) - 0.001
-            G = np.zeros(len(w))
-            base = i
+        if lossFunction == 'MSE':
+            train_err = MSE(y, Y)
 
-        G += dw ** 2
-        w -= lr / np.sqrt(G) * dw
+        if lossFunction == 'LogCosh':
+            train_err = LogCosh(y, Y)
 
-    if visualized:
-        plt.close(point_fig)
-    plt.close(error_fig)
+        print (train_err)
+        print (R2(test_y, test_Y))
+
+        if optimizer == 'adagrad':
+            G += dw ** 2
+            w -= lr / np.sqrt(G) * dw
+
+        if optimizer == 'adam':
+            M = beta_1 * M + (1 - beta_1) * dw
+            V = beta_2 * V + (1 - beta_2) * (dw ** 2)
+            w -= lr * M / (1 - beta_1) / (np.sqrt(V / (1 - beta_2)) + eps)
+
     return w
 
 def p3(train_data, test_data):
     print('Problem 3:')
     keys = list(train_data)
-    while True:
-        X       = Polynomialize(train_data[keys[0:8]].values, 1)
-        Y       = train_data[keys[8]].values.reshape(len(train_data), 1)
-        test_X  = Polynomialize(test_data[keys[0:8]].values, 1)
-        test_Y  = test_data[keys[8]].values.reshape(len(test_data), 1)
+    X       = Polynomialize(train_data[keys[0:8]].values, 1)
+    Y       = train_data[keys[8]].values.reshape(len(train_data), 1)
+    test_X  = Polynomialize(test_data[keys[0:8]].values, 1)
+    test_Y  = test_data[keys[8]].values.reshape(len(test_data), 1)
 
-        w = MVGD(
-            X = X,
-            Y = Y,
-            lr = 0.0001,
-            epoch = 100000,
-            test_X = test_X,
-            test_Y = test_Y,
-            error_min = 500,
-            error_max = 1800,
-            visualized = False
-        )
+    w = MVGD(
+        X = X,
+        Y = Y,
+        lr = 0.001,
+        epoch = 10000,
+        optimizer = 'adam',
+        test_X = test_X,
+        test_Y = test_Y,
+        error_min = 0,
+        error_max = 3000
+    )
 
-        mse = MSE(np.dot(test_X, w).reshape(test_X.shape[0], 1), test_Y)
-        r2 = R2(np.dot(test_X, w).reshape(test_X.shape[0], 1), test_Y)
-
-        if r2 > 0.6:
-            break
+    mse = MSE(np.dot(test_X, w).reshape(test_X.shape[0], 1), test_Y)
+    r2 = R2(np.dot(test_X, w).reshape(test_X.shape[0], 1), test_Y)
 
     print('loss', 'r2', sep = '\t\t')
     print('-------------------------------------------------------------')
@@ -248,27 +252,32 @@ def Polynomialize(data, deg):
         cubic_data = np.concatenate((cubic_data, data ** i), axis=1)
     return cubic_data
 
+def Squared(data):
+    res_data = np.array([[1] for i in range(data.shape[0])])
+    data = np.concatenate((data, res_data), axis = 1)
+    for i in range(data.shape[1]):
+        for j in range(i, data.shape[1]):
+            res_data = np.concatenate((res_data, data[:, i].reshape(data.shape[0], 1) * data[:, j].reshape(data.shape[0], 1)), axis=1)
+    return res_data
+
 def p4(train_data, test_data):
     print('Problem 4:')
     keys = list(train_data)
-    i = 5
-    deg = 2
-    X       = Polynomialize(train_data[keys[i]].values.reshape(len(train_data), 1), deg)
+    X       = Squared(train_data[keys[0:8]].values)
     Y       = train_data[keys[8]].values.reshape(len(train_data), 1)
-    test_X  = Polynomialize(test_data[keys[i]].values.reshape(len(test_data), 1), deg)
+    test_X  = Squared(test_data[keys[0:8]].values)
     test_Y  = test_data[keys[8]].values.reshape(len(test_data), 1)
 
     w = MVGD(
         X = X,
         Y = train_data[keys[8]].values.reshape(len(train_data), 1),
-        lr = 0.0001,
-        epoch = 10000,
+        lr = 100,
+        optimizer = 'adagrad',
+        epoch = 1000000000,
         test_X = test_X,
         test_Y = test_data[keys[8]].values.reshape(len(test_data), 1),
         error_min = 300,
-        error_max = 10000,
-        xlabel = keys[i],
-        ylabel = keys[8]
+        error_max = 10000
     )
 
     mse = MSE(np.dot(test_X, w).reshape(test_X.shape[0], 1), test_Y)
@@ -286,26 +295,22 @@ def p4(train_data, test_data):
 def p5(train_data, test_data):
     print('Problem 5:')
     keys = list(train_data)
-    i = 5
-    deg = 2
-    print('Choose Feature {} and Concrete compressive strength and degree {}'.format(keys[i], deg))
     while True:
-        X       = Polynomialize(train_data[keys[i]].values.reshape(len(train_data), 1), deg)
+        X       = Squared(train_data[keys[0:8]].values)
         Y       = train_data[keys[8]].values.reshape(len(train_data), 1)
-        test_X  = Polynomialize(test_data[keys[i]].values.reshape(len(test_data), 1), deg)
+        test_X  = Squared(test_data[keys[0:8]].values)
         test_Y  = test_data[keys[8]].values.reshape(len(test_data), 1)
 
         w = MVGD(
             X = X,
             Y = train_data[keys[8]].values.reshape(len(train_data), 1),
             lr = 0.0001,
-            epoch = 10000,
+            epoch = 1000000,
+            optimizer = 'adam',
             test_X = test_X,
             test_Y = test_data[keys[8]].values.reshape(len(test_data), 1),
             error_min = 300,
-            error_max = 10000,
-            xlabel = 'Coarse Aggregate',
-            ylabel = 'Concrete compressive strength'
+            error_max = 10000
         )
 
         mse = MSE(np.dot(test_X, w).reshape(test_X.shape[0], 1), test_Y)
@@ -325,7 +330,7 @@ def p5(train_data, test_data):
 
 
 def main():
-    train_data, test_data = load_file('Concrete_Data.csv')
+    train_data, test_data = load_file('Concrete_Data.csv', visualized = False)
     p1(train_data, test_data)
     p2(train_data, test_data)
     p3(train_data, test_data)
