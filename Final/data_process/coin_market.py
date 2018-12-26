c5a6c918925e3e9795ab8a6a61f83ebdf62732a5
Marmot Yang <s1357andy871207@gmail.com>
2018-12-26 13:56:37 +0800

    collect data

--- Final/data_process/coin_market.py
+++ Final/data_process/coin_market.py
@@ -1,8 +1,21 @@
 import requests
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import time
 
-res = requests.get('https://graphs2.coinmarketcap.com/currencies/bitcoin/')
-d = res.json()
-df = pandas.DataFrame(d)
-print (df)
+time.strftime("%Y%m%d", time.localtime())
+res = requests.get('https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130428&end={}'.format(time.strftime("%Y%m%d", time.localtime())))
+soup = BeautifulSoup(res.text, 'html.parser')
+table = soup.find("table", {"class": "table"})
+title = [th.text.replace('\n', '') for th in table.find('tr').find_all('th')]
+table_body = table.find('tbody')
+rows = table_body.find_all('tr')
+data = []
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele])
+
+df = pd.DataFrame(data = data, columns = title)
+df.to_csv('coin_market.csv')
