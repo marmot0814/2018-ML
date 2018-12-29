0191f5ac608c94ae2975f25399144a606a3fad13
Marmot Yang <s1357andy871207@gmail.com>
2018-12-29 17:29:44 +0800

    data_processing

--- Final/data_process/coin_market.py
+++ Final/data_process/coin_market.py
@@ -3,16 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import time
+import datetime
 
 time.strftime("%Y%m%d", time.localtime())
-res = requests.get('https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130428&end={}'.format(time.strftime("%Y%m%d", time.localtime())))
+res = requests.get('https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20131229&end=20181216')
 soup = BeautifulSoup(res.text, 'html.parser')
 table = soup.find("table", {"class": "table"})
 title = [th.text.replace('\n', '') for th in table.find('tr').find_all('th')]
 table_body = table.find('tbody')
 rows = table_body.find_all('tr')
 data = []
+cnt = 6
 for row in rows:
+    cnt += 1
+    cnt %= 7
+    if cnt != 0:
+        continue
     cols = row.find_all('td')
     cols = [ele.text.strip() for ele in cols]
     data.append([ele for ele in cols if ele])
--- Final/data_process/google_trend.py
+++ Final/data_process/google_trend.py
@@ -2,21 +2,30 @@ from pytrends.request import TrendReq
 import json
 import matplotlib.pyplot as plt
 import csv
+import pandas as pd
 
 
 # Hyper parameter
-key_words = ['bitcoin','bit coin', 'virtual currency', 'BTC']
+# key_words = ['bitcoin','bit coin', 'virtual currency', 'BTC']
+key_words = ['bitcoin','BTC']
 show_plot = False
 export_as_csv = True
 ####################
 
-pytrend = TrendReq(hl = 'en-US', tz = 360)
+pytrend = TrendReq(hl = 'en-US')
 pytrend.build_payload(kw_list = key_words, geo = 'TW')
 data = json.loads(pytrend.interest_over_time().to_json(orient='table'))['data'][:-1]
 
 
 ## extract data
-date = [x['date'].split('T')[0] for x in data]
+date = [x['date'].split('T')[0] for x in data][::-1]
+bitcoin = [x['bitcoin'] for x in data][::-1]
+BTC = [x['BTC'] for x in data][::-1]
+df_dict = {'Date': date, 'BTC': BTC, 'bitcoin': bitcoin }
+df = pd.DataFrame.from_dict(df_dict)
+print (df.head())
+df.to_csv('google_trend.csv')
+
 value = []
 
 for key_word in key_words:
@@ -34,9 +43,9 @@ if show_plot:
 		plt.show()
 
 ## export csv
-if export_as_csv:
-	with open('google_trend.csv', 'w') as file:
-		writer = csv.writer(file)
-		writer.writerow(['', *date])
-		for idx in range(len(key_words)):
-			writer.writerow([key_words[idx], *value[idx]])
+# if export_as_csv:
+# 	with open('google_trend.csv', 'w') as file:
+# 		writer = csv.writer(file)
+# 		writer.writerow(['', *date])
+#		for idx in range(len(key_words)):
+#			writer.writerow([key_words[idx], *value[idx]])
