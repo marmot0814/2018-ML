fb75c271fa6b6785a5091145664a0d705ea6629f
Marmot Yang <s1357andy871207@gmail.com>
2018-12-30 23:03:18 +0800

    nn

--- Final/model/nn.py
+++ Final/model/nn.py
@@ -10,7 +10,8 @@ from keras.utils import np_utils
 def load_trend():
     csv = "../data_process/google_trend.csv"
     df = pd.read_csv(csv)[['BTC', 'bitcoin']]
-    return (df - df.mean()) / df.std()
+    df = (df - df.mean()) / df.std()
+    return df
 
 
 def load_coin_mkt():
@@ -18,7 +19,8 @@ def load_coin_mkt():
     df = pd.read_csv(csv).drop("Date", axis = 1).drop('Unnamed: 0', axis = 1)
     df["Market Cap"] = df["Market Cap"].str.replace(',', '').astype(float)
     df["Volume"] = df["Volume"].str.replace(',', '').astype(float)
-    return (df - df.mean()) / df.std()
+    df = (df - df.mean()) / df.std()
+    return df
 
 def load_data(k = 8, ratio = 0.7):
     trend = load_trend().values
@@ -32,15 +34,31 @@ def load_data(k = 8, ratio = 0.7):
     target = []
     for i in range(m - k - 1):
         data.append(data_[i:i + k, DATA_INDEX].flatten())
-        target.append(data_[i+k, TARGET_INDEX])
+        if data_[i+k, TARGET_INDEX] > data_[i+k-1, TARGET_INDEX]:
+            target.append([1, 0])
+        else:
+            target.append([0, 1])
     data = np.array(data)
     target = np.array(target)
 
     x = round(m * ratio)
     train_data = data[:x, :]
-    train_target = data[:x, :]
+    train_target = target[:x, :]
     test_data = data[x:, :]
-    test_target = data[:x, :]
+    test_target = target[x:, :]
     return train_data, train_target, test_data, test_target
 
 train_data, train_target, test_data, test_target = load_data()
+
+model = Sequential()
+model.add(Dense(input_dim = 56, units = 1000, activation='relu'))
+model.add(Dense(units=1000, activation='relu'))
+model.add(Dense(units=1000, activation='relu'))
+model.add(Dense(units=2, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(train_data, train_target, batch_size = 100, epochs=500)
+
+res = model.evaluate(test_data, test_target)
+
+print (res[1])
