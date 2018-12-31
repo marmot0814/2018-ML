fffe85608aab9d8dc5fec8d56bb731b73020979c
Marmot Yang <s1357andy871207@gmail.com>
2018-12-31 16:09:42 +0800

    nn

--- Final/model/nn.py
+++ Final/model/nn.py
@@ -2,95 +2,90 @@ import numpy as np
 np.set_printoptions(threshold=np.nan)
 import pandas as pd
 import datetime
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers.core import Dense, Activation, Dropout
 from keras.optimizers import Adam
 from keras.utils import np_utils
+import random
 
 def load_trend():
     csv = "../data_process/google_trend.csv"
     df = pd.read_csv(csv)[['BTC', 'bitcoin']][::-1]
-    df = (df - df.mean()) / df.std()
     return df
-
 
 def load_coin_mkt():
     csv = "../data_process/coin_market.csv"
     df = pd.read_csv(csv).drop("Date", axis = 1).drop('Unnamed: 0', axis = 1)[::-1]
-    df["Market Cap"] = df["Market Cap"].str.replace(',', '').astype(float)
-    df["Volume"] = df["Volume"].str.replace(',', '').astype(float)
-    df = (df - df.mean()) / df.std()
-    print (df.head())
     return df
 
-def load_data(k, ratio = 0.7):
-    trend = load_trend().values
-    coin = load_coin_mkt().values
-    m = trend.shape[0]
-    data_ = np.concatenate((trend, coin), axis=1)
-    TARGET_INDEX = [5]
-    DATA_INDEX = [i for i in range(np.shape(data_)[1]) if i not in TARGET_INDEX]
+def normalized(df):
+    return (df - df.mean()) / df.std()
+
+def load_data(k, test_number = 8):
+    trend_df = load_trend()
+    coin_df = load_coin_mkt()
+    money = coin_df['Close**'].values
+    trend = normalized(trend_df).values
+    coin = normalized(coin_df.drop('Close**', axis = 1)).values
+    trend_coin = np.concatenate((trend, coin), axis = 1)
+
+    m = trend_coin.shape[0]
     data = []
     target = []
     for i in range(m - k - 1):
-        data.append(data_[i:i + k, DATA_INDEX].flatten())
-        if data_[i+k, TARGET_INDEX] > data_[i+k-1, TARGET_INDEX]:
-            target.append([1, 0])
-        else:
+        data.append(trend_coin[i : i + k, :].flatten())
+        if money[i + k] >= money[i + k + 1]: # 跌
             target.append([0, 1])
-    ordered_data = np.array(data)
-    ordered_target = np.array(target)
+        else:                               # 漲
+            target.append([1, 0])
+    data = np.array(data)
+    target = np.array(target)
 
-    # shuffle
-    idx = np.arange(m - k - 1)
-    np.random.shuffle(idx)
-    data = ordered_data[idx]
-    target = ordered_target[idx]
+    train_data = data[:-test_number, :]
+    train_target = target[:-test_number, :]
 
-    x = round((m - k - 1) * ratio)
-    print ("train: {0}, test: {1}".format(x, m - k - 1 - x))
-    train_data = data[:x, :]
-    train_target = target[:x, :]
-    test_data = data[x:, :]
-    test_target = target[x:, :]
-    return ordered_data, ordered_target, train_data, train_target, test_data, test_target
+    test_data = data[-test_number:, :]
+    test_target = target[-test_number:, :]
+    test_money = money[-test_number - 1:]
 
-def test(model, ordered_data, k, coin = 1):
+    return train_data, train_target, test_data, test_target, test_money
+
+def go(model, test_data, test_money, coin = 10000):
     state = 0   # 0: 沒買, 1: 有買
-    csv = "../data_process/coin_market.csv"
-    money = pd.read_csv(csv)[::-1]['Close**'].values
-    m = ordered_data.shape[0]
     seld = 0
-    for i in range(m):
-        output = model.predict(ordered_data[i, :].reshape(1, ordered_data.shape[1]))
+    for i in range(test_data.shape[0]):
+        output = model.predict(test_data[i, :].reshape(1, test_data.shape[1]))
         res = output.argmax()
         if res == state:
             if state == 0:
-                seld = coin / money[i + k - 1]
+                seld = coin / test_money[i]
                 state = 1
                 coin = 0
             else:
-                coin = seld * money[i + k - 1]
+                coin = seld * test_money[i]
                 state = 0
                 seld = 0
-    return coin + seld * money[-1]
+    return coin + seld * test_money[-1]
 
-
-k = 48 * 2
-ordered_data, ordered_target, train_data, train_target, test_data, test_target = load_data(k, 0.7)
+k = 48
+train_data, train_target, test_data, test_target, test_money = load_data(k, 24)
 
 model = Sequential()
 model.add(Dense(input_dim = 7 * k, units = 1000, activation='relu'))
-model.add(Dropout(0.7122))
+model.add(Dense(units = 1000, activation = 'relu'))
 model.add(Dense(units=2, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(train_data, train_target, batch_size = 100, epochs=1000)
+max_output = -1
+epochs = 200
+while epochs:
+    epochs -= 1
+    model.fit(train_data, train_target, batch_size = 100, epochs=1)
+    output = go(model, test_data, test_money)
+    print (output)
+    if output > max_output:
+        max_output = output
+        model.save('best.h5')
 
-res = model.evaluate(test_data, test_target)
-
-print ("test acc = {0}".format(res[1]))
-
-coin = test(model, ordered_data, k)
-print ("if you throw 1 coin, you will get {0} coin in reward.".format(coin))
-
+model = load_model('best.h5')
+print (go(model, test_data, test_money))
